1dfd0d967a2d5df41b14c70496f806f39e5806f0
ZeltaGamezYT <96065790+ZeltaGamezYT@users.noreply.github.com>
2024-05-27 16:14:12 -0500

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,56 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Outlook Inbox", url: "https://outlook.live.com/mail/0/" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "OneDrive", url: "https://onedrive.live.com" },
+      { name: "Microsoft 365", url: "https://microsoft365.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "YouTube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      {
+        name: "Amazon Prime",
+        url: "https://www.amazon.com/Amazon-Video",
+      },
+      { name: "Apple Music", url: "https://music.apple.com" },
+      { name: "SoundCloud", url: "https://soundcloud.com" },
+    ],
+  },
+  {
+    title: "YouTube Channels",
+    links: [
+      { name: "LetsGameItOut", url: "https://www.youtube.com/@LetsGameItOut/featured" },
+      { name: "Mark Rober", url: "https://www.youtube.com/@MarkRober/featured" },
+      { name: "DIY Perks", url: "https://www.youtube.com/@DIYPerks/featured" },
+      { name: "Corridor Crew", url: "https://www.youtube.com/@CorridorCrew/featured" },
+      { name: "Knarfy", url: "https://www.youtube.com/@Knarfy/featured" },
+      { name: "SystemZee", url: "https://www.youtube.com/@syszee/featured" },
+    ],
+  },
+  {
+    title: "Video Games",
+    links: [
+      { name: "Classic Minecraft", url: "https://classic.minecraft.net" },
+      { name: "Roblox", url: "https://roblox.com" },
+      { name: "Diep.io", url: "https://diep.io" },
+      { name: "SmashKarts", url: "https://smashkarts.io" },
+      { name: "Slither.io", url: "https://slither.io" },
+    ],
+  },
+  {
+    title: "Forums",
+    links: [
+      { name: "Quora", url: "https://quora.com" },
+      { name: "Minecraft Forums", url: "https://minecraftforum.net" },
+      { name: "StackOverflow", url: "https://stackoverflow.com" },
+      { name: "StackExchange", url: "https://stackexchange.com" },
+      { name: "Slither.io", url: "https://slither.io" },
+    ],
+  },
+];
